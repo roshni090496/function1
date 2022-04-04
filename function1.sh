456ac64bb3685d3c457636f90561ee28e32ab298
roshni090496 <rs09041001@gmail.com>
2022-04-03 20:07:35 -0700

    function programme

---
+++ function1.sh
@@ -0,0 +1,15 @@
+#Power Function
+
+function power()
+{
+       num=$1
+       p=$2
+       pow=1
+      for(( i=$p ; i>0 ; i--))
+      do
+      pow=$(( $pow * $num ))
+      done
+      echo $pow
+
+}
+power 2 3
